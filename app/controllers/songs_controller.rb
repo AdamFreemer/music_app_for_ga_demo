be8ea6e0c213cadb06f8540aef46b42982396299
Adam Freemer <afreemer@datapipe.com>
2015-11-16 13:34:43 -0500

    Refresh

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,7 @@ class SongsController < ApplicationController
   # GET /songs.json
   def index
     @songs = Song.paginate(:page => params[:page], :per_page => 5)
+    @test = 'test'
   end
 
   # GET /songs/1
